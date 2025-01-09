434e9ebc16aca9cec958da4191e5b85d95346790
Mardvey-UMA <chikernut213@gmail.com>
2025-01-10 02:30:45 +0400

    Add Entity

---
+++ auth-service/src/main/kotlin/ru/dating/authservice/entity/MailToken.kt
@@ -0,0 +1,23 @@
+package ru.dating.authservice.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import java.time.LocalDateTime
+
+@Entity
+class MailToken(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null,
+    var token: String,
+    var expiresAt: LocalDateTime,
+    var createdAt: LocalDateTime,
+    var validatedAt: LocalDateTime? = null,
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false) var user: User
+)
