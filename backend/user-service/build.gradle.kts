57f7d1721eaaed67da7b2abce65af31418f0f1ff
Mardvey-UMA <chikernut213@gmail.com>
2025-02-02 22:28:13 +0400

    fix eureka server

--- backend/user-service/build.gradle.kts
+++ backend/user-service/build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
 
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    //implementation("org.springframework.cloud:spring-cloud-starter-gateway")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
